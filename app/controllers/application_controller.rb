90a0286a7a4682a938b1c35ce52e0bbe63d7720c
Andre <andregomesngr@gmail.com>
2020-06-01 19:47:30 -0500

    Configurando o template de Admin Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  layout :resouve_layout
+
+  protected
+    def resouve_layout
+      if devise_controller? && resource_class == Admin
+        'admin_devise'
+      else
+        'application'
+      end
+    end
 end
