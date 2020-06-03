49245c9863e22d11b76c848db0d33b2f81f24797
Andre <andregomesngr@gmail.com>
2020-06-02 22:26:31 -0500

    Ativando as respostas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  namespace :site do    
+    get 'welcome/index'
+    get 'search', to: 'search#questions'
+    post 'answer', to: 'answer#question'
+  end
   namespace :admins_backoffice do
     resources :admins, except: [:show]
     resources :subjects, except: [:show]
     resources :questions, except: [:show]
     get 'welcome/index'
-  end
-  namespace :site do
-    get 'welcome/index'
-    get 'search', to: 'search#questions'
   end
   namespace :users_backoffice do
     get 'welcome/index'
