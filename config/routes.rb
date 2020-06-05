803d77f8eab7648c78b10d3a5611bfc63434c150
Andre <andregomesngr@gmail.com>
2020-06-05 00:41:11 -0500

    Adicionando um Janela Modal

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registration]
   devise_for :users
 
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
+
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
