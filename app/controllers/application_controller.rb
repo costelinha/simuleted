dfaaad7c35f2a7cf29cc9a5f15cfd8581b285056
Andre <andregomesngr@gmail.com>
2020-06-05 16:30:17 -0500

    Bloquenado paginacao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   layout :resource_layout
+  before_action :check_pagination
 
   protected
     def resource_layout
       devise_controller? ? "#{resource_class.to_s.downcase}_devise" : 'application'
+    end
+
+    def check_pagination
+      unless user_signed_in?
+        params.extract!(:page)
+      end
     end
 end
